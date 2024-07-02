629992201826d3db7cb541b4d9f7cdbd498a7943
kevappdev <kevappdev@gmail.com>
2024-07-02 19:52:07 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
+import {HeaderComponent} from "./components/common/header/header.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LandingPageComponent
+  },
+  {
+    path: "test",
+    component: HeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
---
+++ src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent {
+
+  public textVariable:string = "Dies ist ein String";
+
+
+  public listeAnStrings = [
+    "sfgdf",
+    "jkghhj",
+    "1234",
+    "fggdfhfg",
+    "sdfdsfgsgd",
+    "ssdasd",
+    "sdfsd",
+  ];
+
+}
