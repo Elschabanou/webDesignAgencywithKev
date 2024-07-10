e995a672a20f1feb3f7dead0cf318517c6d706b2
Felix Schabana <felix.schabana@porsche.de>
2024-07-10 13:56:23 +0200

    tidied up and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { HeaderComponent } from './components/common/header/header.component';
+import { MainButtonComponent } from './main-button/main-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-button/main-button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-main-button',
+  templateUrl: './main-button.component.html',
+  styleUrls: ['./main-button.component.scss']
+})
+export class MainButtonComponent {
+  @Input() link: string = '#';
+}
